55d046842aaf0df5077d959ffab8118372795572
cljzxx <9898910@qq.com>
2024-01-30 10:34:44 +0800

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
+    // 网格布局（注意双大括号）
     <Grid
       templateAreas={{
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}>
-      <GridItem area='nav' bg='coral'>
-        Nav
+      {/* 导航区域及背景颜色 */}
+      <GridItem area='nav'>
+        <NavBar />
       </GridItem>
+      {/* Show组件根据条件判断显示 */}
       <Show above='lg'>
+        {/* 侧边栏区域及背景颜色 */}
         <GridItem area='aside' bg='gold'>
           Aside
         </GridItem>
       </Show>
+      {/* 主体区域及背景颜色 */}
       <GridItem area='main' bg='dodgerblue'>
         main
       </GridItem>
