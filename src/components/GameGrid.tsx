2c18cd7bfc6003e61549a743f75dc49536c16d4b
cljzxx <9898910@qq.com>
2024-01-31 09:58:16 +0800

    Create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,30 +1,9 @@
-import { useEffect, useState } from 'react'
-import apiClient from '../services/api-client'
 import { Text } from '@chakra-ui/react'
-// 定义游戏对象界面
-interface Game {
-  id: number
-  name: string
-}
-// 定义响应数据界面
-interface FetchGamesResponse {
-  count: number
-  results: Game[] // 游戏对象数组
-}
+import useGames from '../hooks/useGames'
+
 // 游戏网格布局函数
 const GameGrid = () => {
-  // 存储游戏列表/更新游戏列表/默认空数组
-  const [games, setGames] = useState<Game[]>([]) // 界面类型游戏对象数组
-  // 存储错误/更新错误/默认空字符串
-  const [error, setError] = useState('')
-  // 渲染后效果钩子
-  useEffect(() => {
-    // 自定义的请求组件
-    apiClient
-      .get<FetchGamesResponse>('/games') // 请求游戏列表端点/响应的界面类型
-      .then(res => setGames(res.data.results)) // 更新游戏列表
-      .catch(err => setError(err.message)) // 异常时错误信息
-  })
+  const { games, error } = useGames()
   // 返回视图
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import apiClient from '../services/api-client'
+import { CanceledError } from 'axios'
+// 定义游戏对象界面
+interface Game {
+  id: number
+  name: string
+}
+// 定义响应数据界面
+interface FetchGamesResponse {
+  count: number
+  results: Game[] // 游戏对象数组
+}
+// 游戏状态组件（hook）
+const useGames = () => {
+  // 存储游戏列表/更新游戏列表/默认空数组
+  const [games, setGames] = useState<Game[]>([]) // 界面类型游戏对象数组
+  // 存储错误/更新错误/默认空字符串
+  const [error, setError] = useState('')
+
+  // 渲染后效果钩子 (注意[]结尾)
+  useEffect(() => {
+    // 取消请求控制器
+    const controller = new AbortController()
+    // 自定义的请求组件/请求游戏列表端点/响应的界面类型
+    apiClient
+      .get<FetchGamesResponse>('/games', { signal: controller.signal }) // 捕捉信道
+      .then(res => setGames(res.data.results)) // 更新游戏列表
+      .catch(err => {
+        // 如果err实例->来自于->错误类
+        if (err instanceof CanceledError) return
+        setError(err.message) // 异常时错误提示
+      })
+    // 取消请求
+    return () => controller.abort()
+  }, [])
+  // 返回对象（属性及方法）
+  return { games, error }
+}
+// 默认导出hook
+export default useGames
