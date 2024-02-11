8242da23c8515e4d22e48aeca38857b1d73e1b08
cljzxx <9898910@qq.com>
2024-02-11 14:20:48 +0800

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
-import useData from './useData'
+// import useData from './useData'
+// 获取本地静态资源
+import genres from '../data/genres'
 // 定义流派对象界面
 export interface Genre {
   id: number
@@ -6,6 +8,8 @@ export interface Genre {
   image_background: string // 新增图片字段
 }
 // 流派钩子组件（返回通用数据钩子/流派类型/流派端点）
-const useGenres = () => useData<Genre>('/genres')
+// const useGenres = () => useData<Genre>('/genres')
+// 返回静态对象资源
+const useGenres = () => ({ data: genres, isLoading: false, error: null })
 
 export default useGenres
