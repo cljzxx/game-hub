6a4d5e622fdc01f349b7c88ee8b49ce11b12019d
cljzxx <9898910@qq.com>
2024-02-11 17:24:12 +0800

    Refactor GameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,8 +3,6 @@ import useGames from '../hooks/useGames'
 import GameCard from './GameCard'
 import GameCardSkeleton from './GameCardSkeleton'
 import GameCardContainer from './GameCardContainer'
-import { Genre } from '../hooks/useGenres'
-import { Platform } from '../hooks/usePlatforms'
 import { GameQuery } from '../App'
 
 // 参数对象
@@ -18,34 +16,34 @@ const GameGrid = ({ GameQuery }: Props) => {
   const { data, error, isLoading } = useGames(GameQuery)
   // 为骨架屏模拟的六组数据
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+  // 如果有错误则显示错误信息（JSX）
+  if (error) return <Text>{error}</Text>
+
   // 返回视图
   return (
-    <>
-      {/* 如果有错误则显示错误提示 */}
-      {error && <Text>{error}</Text>}
-      {/* 简易网格布局/根据屏幕大小控制列数/内边距/间距 */}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding='10px'
-        // 间距由10变成3
-        spacing={6}>
-        {/* 加载中则显示骨架屏 */}
-        {isLoading &&
-          skeletons.map(skeleton => (
-            // 容器组件包裹（注意key值）
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map(game => (
+    // 简易网格布局/根据屏幕大小控制列数/内边距/间距
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding='10px'
+      // 间距
+      spacing={6}>
+      {/* 加载中则显示骨架屏 */}
+      {isLoading &&
+        skeletons.map(skeleton => (
           // 容器组件包裹（注意key值）
-          <GameCardContainer key={game.id}>
-            {/* 游戏卡片组件/传参 */}
-            <GameCard game={game} />
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map(game => (
+        // 容器组件包裹（注意key值）
+        <GameCardContainer key={game.id}>
+          {/* 游戏卡片组件/传参 */}
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   )
 }
 export default GameGrid
