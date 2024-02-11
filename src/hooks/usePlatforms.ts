54fd5b06d5e1c11b0f09e0c8aa8e04a8a8ff5651
cljzxx <9898910@qq.com>
2024-02-11 16:13:41 +0800

    Ship platforms with the app

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,6 @@
-import useData from './useData'
+// import useData from './useData'
+// 获取静态数据
+import platforms from '../data/platforms'
 // 定义接口参数及类型
 export interface Platform {
   id: number
@@ -6,6 +8,9 @@ export interface Platform {
   slug: string
 }
 // 定义平台钩子组件（调用通用数据钩子及获取数据类型）
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+// const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
+// 返回静态数据
+const usePlatforms = () => ({ data: platforms, error: null })
+
 // 导出默认钩子组件
 export default usePlatforms
